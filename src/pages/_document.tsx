d064953b5694c6461fc7cbb7f3d5dc22c126dc7d
William Villeneuve <ciiqr@users.noreply.github.com>
2023-05-07 14:10:43 -0400

    fix height

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
     return (
-        <Html>
+        <Html className="h-full">
             <Head>
                 <link rel="icon" href="/favicon.png" />
             </Head>
